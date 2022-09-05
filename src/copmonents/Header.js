7556e8a71df44f619f6b6fb1c06351d8b6b3e218
Dmitry Rakovich <dmitryrakovich@mail.ru>
2022-09-05 20:39:11 +0300

    fix: change object on number in request parameter

--- src/copmonents/Header.js
+++ src/copmonents/Header.js
@@ -1,12 +1,9 @@
 function Header({getFilm, setFilm}) {
     
-  // const pressButton = (e) => {
-  //   getFilm()
-  // }
   return (
     <header className='header'>
     <p>Movie Catalog</p>
-    <input onKeyUp={getFilm} onChange={(e)=> setFilm(e.target.value)} placeholder='enter more than two characters' type='search' />
+    <input onKeyUp={() => getFilm(1)} onChange={(e)=> setFilm(e.target.value)} placeholder='enter more than two characters' type='search' />
     <div className='header_user'>
     <img src="https://img.icons8.com/ios-filled/50/000000/user.png" width={16} height={16} alt='user-pic'/>
     <p>Dmitry Rakovich</p>
