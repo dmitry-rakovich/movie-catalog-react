e0670754802d51b1b69f053eb28616f823df4c54
Dmitry Rakovich <dmitryrakovich@mail.ru>
2022-09-14 21:52:52 +0300

    fix: pagination

--- src/copmonents/Pages.js
+++ src/copmonents/Pages.js
@@ -2,14 +2,21 @@ import { useState, useEffect } from "react"
 
 
 function Pages({ getFilm, result }) {
-  const [thisPage, setThisPage] = useState(0)
+  const [thisPage, setThisPage] = useState(1)
   const [pages, setArrPages] = useState([])
 
   const handleGetFilm = (page) => {
     getFilm(page)
     setThisPage(page)
   }
+  const showPages = () => {
+    const arr = new Array(Math.ceil(result/10)).fill(1, 0, Math.ceil(result/10)).map((el, i) => el += i)
+    setArrPages(arr.slice(thisPage-1, thisPage+9));
+  }
 
+  useEffect(() => {
+    showPages()
+  }, [thisPage]);
   return (
     <div className='pages'>
       {thisPage > 1 ? 
@@ -23,7 +30,7 @@ function Pages({ getFilm, result }) {
         </button>
       )
     })}
-    {result <= 10 ?
+    {pages.length < 10 ?
     <button style={{cursor:'no-drop'}} disabled='disabled' onClick={()=>handleGetFilm(thisPage + 1)}>▶️</button> :
     <button onClick={()=>handleGetFilm(thisPage + 1)}>▶️</button>}
     </div>
